3e0fbcec3740baace513f84a80794d434a255288
Ashish Sharda <ashishjsharda@gmail.com>
2021-07-21 06:47:58 -0400

    Add all my files

---
+++ src/test/java/com/ashish/julydemo/JulydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.julydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
