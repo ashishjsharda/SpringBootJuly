294fff1362da739746817c96a81fd5f03f47f317
Ashish Sharda <ashishjsharda@gmail.com>
2021-07-21 08:06:23 -0400

    Add Unit Test

---
+++ src/test/java/com/ashish/julydemo/controllers/HelloControllerUnitTest.java
@@ -0,0 +1,19 @@
+package com.ashish.julydemo.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+import org.springframework.validation.support.BindingAwareModelMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloControllerUnitTest {
+
+    @Test
+    public void testHello()
+    {
+        HelloController helloController=new HelloController();
+        Model model=new BindingAwareModelMap();
+        String result=helloController.sayHello("hello",model);
+        assertEquals("Hello",result);
+    }
+}
